45dad3df34028b2005071b39208760e8a152765d
Eduardo Nascimento <jebnascimento@outlook.com>
2022-10-24 20:50:22 -0300

    create SideBar component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,44 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { GenreResponseProps } from "../App";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+interface ISideBarProps {
+  genres: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }[],
+  selectedId: number,
+  setSelectedId: ( id: number) => void;
+}
+
+export function SideBar({genres, selectedId, setSelectedId}: ISideBarProps ) {
+
+  useEffect(() => {
+    api.get<GenreResponseProps>(`genres/${selectedId}`).then(response => {
+      setSelectedId(response.data.id);
+    })
+  }, [selectedId]);
+
+  function handleClickButton(id: number) {
+    setSelectedId(id);
+  }
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedId === genre.id}
+            />
+          ))}
+        </div>
+    </nav>
+  )
 }
